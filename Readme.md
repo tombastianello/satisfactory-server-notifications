2d673a1bb29bd3750d019d7f4d0920b92e31bd9f
Tommaso Bastianello <tom.bastianello@gmail.com>
2022-02-03 16:02:45 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,71 @@
+## Satisfactory Server Notification
+Simple application intended to be used as a sidecar to the satisfactory dedicated server container. This can also be run manually or as a service, although config examples are not provided at the moment.
+
+### So what does it do?
+For the moment functionality is basic, however, this can be expanded in the future. Currently this will notify a discord channel via webhook when users connect or disconnect from the server.
+
+### WARNING
+This is still really hacky at the moment and I can't promise it will work reliably.
+
+### Configuration
+To configure the application, use the following 2 environment variables:
+- **LOG_PATH**: Directory where the logs are stored, defaults to `/config/gamefiles/FactoryGame/Saved/Logs`, this is the default for the docker deployment using the `wolveix/satisfactory-server` image.
+- **WEBHOOK_PATH**: URL path of the discord webhook, in the following format: `/api/webhooks/<wh_id>/<wh_token>`.
+
+### Kubernetes Example
+Example of using this as a sidecar for the `wolveix/satisfactory-server` container.
+```yaml
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: satisfactory
+  namespace: game-servers
+spec:
+  strategy:
+    type: Recreate
+  selector:
+    matchLabels:
+      app: satisfactory
+  template:
+    metadata:
+      labels:
+        app: satisfactory
+    spec:
+      hostNetwork: true
+      containers:
+      - name: satisfactory
+        env:
+          - name: MAXPLAYERS
+            value: "6"
+          - name: STEAMBETA
+            value: "false"
+        image: wolveix/satisfactory-server:latest
+        resources:
+          requests:
+            cpu: "1000m"
+            memory: "4Gi"
+          limits:
+            cpu: "2000m"
+            memory: "8Gi"
+        ports:
+        - containerPort: 15777
+        - containerPort: 15000
+        - containerPort: 7777
+        volumeMounts:
+            - name: data
+              mountPath: /config
+      - name: discord-notifications
+        env:
+          - name: WEBHOOK_PATH
+            value: "/api/webhooks/<wh_id>/<wh_token>"
+        image: ghcr.io/tombastianello/satisfactory-server-notifications:latest
+        volumeMounts:
+            - name: data
+              mountPath: /config
+      volumes:
+        - name: data
+          hostPath:
+            path: /path/to/satisfactory/dir
+            type: Directory
+```
---
+++ index.ts
@@ -0,0 +1,99 @@
+import { readFileSync, watchFile } from "fs";
+import * as path from "path";
+import * as https from "https";
+
+const gameLogPath = process.env.LOG_PATH || "/config/gamefiles/FactoryGame/Saved/Logs";
+const webhookPath = process.env.WEBHOOK_PATH;
+if(!(webhookPath)) {
+    throw new Error("Please specify a Discord Webhoom path in the following format: /api/webhooks/<wh_id>/<wh_token>");
+}
+
+const notifyDiscord = (message: string) => {
+    var options = {
+        'method': 'POST',
+        'hostname': 'discord.com',
+        'path': webhookPath,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'maxRedirects': 20
+    };
+
+    var req = https.request(options, (res) => {
+        var chunks: any = [];
+
+        res.on("data", (chunk) => {
+            chunks.push(chunk);
+        });
+
+        res.on("end", (chunk: any) => {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
+
+        res.on("error", function (error) {
+            console.error(error);
+        });
+    });
+
+    var postData = JSON.stringify({
+        "content": message
+    });
+
+    req.write(postData);
+
+    req.end();
+}
+
+const logFile = path.join(gameLogPath, "/FactoryGame.log");
+let fileLinesLenght = readFileSync(logFile, "utf-8").toString().split("\n").length;
+
+console.log(`[${new Date().toLocaleString()}] Starting watcher for '${logFile}'. Lines at start: ${fileLinesLenght}`);
+
+interface User {
+    playerName: string;
+    playerIP: string;
+}
+
+let users: User[] = [];
+let pendingIP: string;
+
+watchFile(logFile, () => {
+    let newLines = readFileSync(logFile, "utf-8").toString().split("\n");
+
+    let deltaLines = newLines.length - fileLinesLenght;
+    fileLinesLenght = newLines.length;
+    //console.log(`[${new Date().toLocaleString()}] ${logFile} has been updated. Lines: ${newLines.length} | Delta: ${deltaLines}`);
+    for (let i = fileLinesLenght - deltaLines; i < fileLinesLenght; i++) {
+        if (newLines[i].includes("Join succeeded:")) {
+            let playerName = newLines[i].split("Join succeeded:")[1].trim();
+            console.log(`[${new Date().toLocaleString()}] Player '${playerName}' joined from '${pendingIP}'.`);
+            notifyDiscord(`**${playerName}** joined the server.`);
+            users.push({
+                playerName: playerName,
+                playerIP: pendingIP
+            });
+            pendingIP = "";
+        }
+        if (newLines[i].includes("AddClientConnection: Added client connection")) {
+            let matchResult = newLines[i].match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/gm);
+            if (matchResult) {
+                pendingIP = matchResult[0];
+            }
+        }
+        if (newLines[i].includes("UNetConnection::Close")) {
+            let disconnectIP = "";
+            let matchResult = newLines[i].match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/gm);
+            if (matchResult) {
+                disconnectIP = matchResult[0];
+            }
+            let disconnectedUser = users.filter(user => user.playerIP === disconnectIP)[0];
+            if(disconnectedUser) {
+                console.log(`[${new Date().toLocaleString()}] Player '${disconnectedUser.playerName}' disconnected from '${disconnectedUser.playerIP}'.`);
+                notifyDiscord(`**${disconnectedUser.playerName}** left the server.`);
+                users.splice(users.indexOf(disconnectedUser), 1);
+            }
+        }
+    }
+});
+
